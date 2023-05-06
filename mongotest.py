8668678376962e001b44acf40b318dad46b075b5
ISHANSINGH <thapaishan1995@gmail.com>
2023-05-06 19:04:04 +0530

    This is my second code push in git.

--- mongotest.py
+++ mongotest.py
@@ -10,4 +10,27 @@ d = {
 }
 db1 = client['mongotest']
 coll = db1['test']
+<<<<<<< HEAD
+=======
+coll.insert_one(d )
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )db1 = client['mongotest']
+coll = db1['test']
+>>>>>>> 39dd41f (This is my second code push in git.)
 coll.insert_one(d )
